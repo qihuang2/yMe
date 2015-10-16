763b934f56733946f2bf45bf80ea69224c48911c
Qi Feng Huang <qifenghuang2@gmail.com>
2015-10-16 12:28:09 -0400

    initial commit

---
+++ yMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  yMe
+//
+//  Created by Qi Feng Huang on 10/16/15.
+//  Copyright © 2015 hackathon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
